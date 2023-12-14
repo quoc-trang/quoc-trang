15494b9afaf99e47d13c3416687fffb64db55a55
trang <trang.lq@raksul.com>
2023-12-14 11:26:02 +0700

    buil: setup repo

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      Montserrat: true,
+    },
+  },
+  css: ["~/assets/css/main.css"],
+});
