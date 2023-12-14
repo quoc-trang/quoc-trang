a64df3e33feca6cea71fa756de09e8fd1f623d3a
trang <trang.lq@raksul.com>
2023-12-14 15:59:40 +0700

    feat: introduction

---
+++ constants/data.ts
@@ -0,0 +1,18 @@
+export const SOCIALS = [
+  {
+    icon: "ri:twitter-x-fill",
+    name: "Twitter",
+    url: "https://twitter.com/TRANGQUOC12",
+  },
+  {
+    icon: "mdi:instagram",
+    name: "Instagram",
+    url: "https://www.instagram.com/quoctrangg/?hl=en",
+  },
+  { icon: "mdi:github", name: "Github", url: "https://github.com/quoc-trang" },
+  {
+    icon: "ic:baseline-tiktok",
+    name: "Tiktok",
+    url: "https://www.tiktok.com/@vanlaanh_2k",
+  },
+];
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,55 @@
 <template>
-  <section class="w-screen h-screen flex justify-center items-start px-4">
-    <div class="mt-40 flex flex-col gap-y-2">
-      <p>Hi ✌️,</p>
-      <p>
-        My full name is <span class="fav-border">Lê Quốc Trạng</span>, I'm a
-        Frontend developer
-      </p>
-      <p>
-        Outside of programming, I enjoy staying with my family, if you're here
-        please follow my
+  <section class="w-screen h-screen flex flex-col items-center px-4">
+    <div class="mt-32 md:mt-40">
+      <h1 class="font-bold inline-block text-3xl">Lê Quốc Trạng</h1>
+      <p class="leading-6 md:leading-8 mt-10">
+        Hi ✌️,<br />
+        I'm a Frontend developer currently working at
+        <NuxtLink
+          to="https://recruit-vn.raksul.com/"
+          target="_blank"
+          class="fav-border font-bold hover:opacity-80 duration-300"
+          >Raksul Viet Nam</NuxtLink
+        >
+        <br />
+        Outside of programming, I enjoy staying with my family<br />
+        if you're here please follow my
         <NuxtLink
           to="https://www.tiktok.com/@vanlaanh_2k"
           target="_blank"
-          class="fav-border"
-          >Tiktok<br
-        /></NuxtLink>
+          class="fav-border font-bold hover:opacity-80 duration-300"
+          >Tiktok,</NuxtLink
+        >
         I want to be famous 😂
       </p>
+      <div class="mt-10 md:mt-20">
+        <h1>Find me on</h1>
+        <ul class="flex gap-x-5 mt-2">
+          <li
+            v-for="(social, index) in SOCIALS"
+            :key="index"
+            class="hover:text-primary cursor-pointer duration-300"
+          >
+            <NuxtLink
+              :to="social.url"
+              target="_blank"
+              class="flex justify-center items-center"
+            >
+              <div class="block md:hidden">
+                <Icon :name="social.icon" size="2em" />
+              </div>
+              <div class="hidden md:block">
+                <Icon :name="social.icon" size="1.5em" />
+              </div>
+              <span class="ml-2 hidden md:block">{{ social.name }}</span>
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
+
+<script setup lang="ts">
+import { SOCIALS } from "~/constants/data";
+</script>
