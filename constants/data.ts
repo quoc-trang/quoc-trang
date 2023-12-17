97018ff6fc8d25b7bb6e57a3d99f98f622a8b674
trang <trang.lq@raksul.com>
2023-12-17 11:27:38 +0700

    feat: add place checkin

--- constants/data.ts
+++ constants/data.ts
@@ -34,3 +34,58 @@ export const NAVBAR = [
   //   text: "Projects",
   // },
 ];
+
+export const CHECK_IN_PLACES = [
+  "  Vườn Hồng Trên Mây QL 20, Trường An, Xuân Trường ",
+
+  "Đường Cô Bắc  ",
+
+  "Nhà thờ Don Bosco Đường tàu Trần Quý Cáp ",
+
+  "Tua Bin Điện Gió Hồ Xuân Hương/Tuyền Lâm ",
+
+  "Đập Ankroet",
+
+  "Đồi chè Cầu Đất",
+
+  "Đồi Đa Phú, Đồi Ngô Quyền Đồi Robin, Đồi Thiên Phúc Đức ",
+
+  "Các cung đường, con dốc ",
+
+  "Đồi cỏ hồng, cỏ cháy ",
+
+  " Dinh tỉnh trưởng Quảng Trường Lâm Viên",
+
+  "Chợ Đà Lạt - Khu Hòa Bình Cây Thông Cô Đơn",
+
+  "Nhà Thờ Domaine de Marie ",
+
+  "Xóm Lèo Huỳnh Tấn Phát ",
+
+  "Sân Vận Động",
+
+  "  Đường hoa tím (Phê La) Nguyễn Chí Thanh",
+
+  "  Suối tía",
+
+  " Biệt thự hoa hồng • Chùa Linh Phước",
+];
+
+export const DATE1 = [
+  "8h00: Phở Atiso - 45k - H19 - H2O, KQH Phạm Hồng Thái, phường 10",
+  "9h00: Check-in Giáng sinh ở Yooberi - 100k - 27 Trịnh Hoài Đức, phường 11",
+  "11h00: Ăn trưa tại Tiệm gà Túk Túk - 160k - 39 Trần Đại Nghĩa, phường 8",
+  "12h30: Về homestay nghỉ trưa - 200-250k/người",
+  "14h30: Thăm thủ ở Chika Farm - 100k - Đa Quý, Xuân Thọ 16h00: Tiện đường ghé qua Vườn Hồng Trên Mây - Miễn phí",
+  "19h00: Đến Túi nướng Lavender ăn tối - 160k - 17/6 Nguyễn Trãi, phường 9",
+  "21h00: Đi dạo, ngắm phố xá, nghe violin ở Khu Hòa Bình",
+  "22h00: Nhâm nhi cocktail ở ABIT BAR & BISTRO - 61 Nguyễn Trãi, phường 9",
+];
+
+export const DATE2 = [
+  "6h30: Cà phê sáng ở Quyen kế biệt thự cổ - 20k - 7 Cô Giang",
+  "7h30: Ăn sáng nhẹ nhàng bánh mỳ chảo - 25k - 303 Hai Bà Trưng, Phường 6",
+  "8h30: Cà phê picnic siêu chill ở The Camp Inn - 65k - Chân đèo Mimosa",
+  "10h00: Chạy xe 1 vòng hóng mát, ghé qua dốc Nhớ Hoài",
+  "11h30: Ăn trưa ở Chợ Phiên Quán - 108 Vạn Hạnh, P8 12h30: Check-out homestay",
+];
--- pages/travels/index.vue
+++ pages/travels/index.vue
@@ -7,7 +7,18 @@
       <h1>Điểm thuê xe máy</h1>
     </section>
     <section class="text-block">
-      <h1>Địa điểm check-in miễn phí</h1>
+      <h1 class="font-bold text-xl border-b border-primary">
+        Địa điểm check-in miễn phí
+      </h1>
+      <div class="flex flex-col gap-y-2 mt-5">
+        <p
+          v-for="item in CHECK_IN_PLACES"
+          :key="item"
+          class="text-xs md:text-sm border-l border-primary pl-2"
+        >
+          {{ item }}
+        </p>
+      </div>
     </section>
     <section class="text-block">
       <h1>Quán cà phê thiên nhiên view đẹp</h1>
@@ -21,24 +32,24 @@
     <section class="text-block">
       <h1>Mua gì làm quà?, địa điểm?</h1>
     </section>
-    <section class="text-block p-5">
+    <section class="text-block">
       <h1 class="text-2xl font-bold">Gợi ý lịch trình</h1>
 
       <div class="text-xs md:text-sm leading-6 mt-5">
         <h1 class="font-bold text-xl border-b border-primary mb-5">Ngày 1</h1>
         <ol>
-          <li class="time" v-for="item in date1" :key="item">{{ item }}</li>
+          <li class="time" v-for="item in DATE1" :key="item">{{ item }}</li>
         </ol>
       </div>
     </section>
 
-    <section class="text-block p-5">
+    <section class="text-block">
       <h1 class="text-2xl font-bold">Gợi ý lịch trình</h1>
 
       <div class="text-xs md:text-sm leading-6 mt-5">
         <h1 class="font-bold text-xl border-b border-primary mb-5">Ngày 2</h1>
         <ol>
-          <li class="time" v-for="item in date2" :key="item">{{ item }}</li>
+          <li class="time" v-for="item in DATE2" :key="item">{{ item }}</li>
         </ol>
       </div>
     </section>
@@ -46,29 +57,12 @@
 </template>
 
 <script setup lang="ts">
-const date1 = [
-  "8h00: Phở Atiso - 45k - H19 - H2O, KQH Phạm Hồng Thái, phường 10",
-  "9h00: Check-in Giáng sinh ở Yooberi - 100k - 27 Trịnh Hoài Đức, phường 11",
-  "11h00: Ăn trưa tại Tiệm gà Túk Túk - 160k - 39 Trần Đại Nghĩa, phường 8",
-  "12h30: Về homestay nghỉ trưa - 200-250k/người",
-  "14h30: Thăm thủ ở Chika Farm - 100k - Đa Quý, Xuân Thọ 16h00: Tiện đường ghé qua Vườn Hồng Trên Mây - Miễn phí",
-  "19h00: Đến Túi nướng Lavender ăn tối - 160k - 17/6 Nguyễn Trãi, phường 9",
-  "21h00: Đi dạo, ngắm phố xá, nghe violin ở Khu Hòa Bình",
-  "22h00: Nhâm nhi cocktail ở ABIT BAR & BISTRO - 61 Nguyễn Trãi, phường 9",
-];
-
-const date2 = [
-  "6h30: Cà phê sáng ở Quyen kế biệt thự cổ - 20k - 7 Cô Giang",
-  "7h30: Ăn sáng nhẹ nhàng bánh mỳ chảo - 25k - 303 Hai Bà Trưng, Phường 6",
-  "8h30: Cà phê picnic siêu chill ở The Camp Inn - 65k - Chân đèo Mimosa",
-  "10h00: Chạy xe 1 vòng hóng mát, ghé qua dốc Nhớ Hoài",
-  "11h30: Ăn trưa ở Chợ Phiên Quán - 108 Vạn Hạnh, P8 12h30: Check-out homestay",
-];
+import { CHECK_IN_PLACES, DATE1, DATE2 } from "~/constants/data";
 </script>
 
 <style scoped>
 .text-block {
-  @apply grid place-content-start min-h-screen snap-start pt-14;
+  @apply grid place-content-start min-h-screen snap-start pt-14 p-5;
 
   .time {
     @apply border-l border-primary pl-2 my-2;
